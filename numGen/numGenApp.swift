1ab6545c3e24c5082d51b06d46bcaebd38f565fe
Annie Gardner <anniewilson@vmware.com>
2021-02-25 11:12:20 -0500

    initial

---
+++ numGen/numGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  numGenApp.swift
+//  numGen
+//
+//  Created by Annie Gardner on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct numGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
